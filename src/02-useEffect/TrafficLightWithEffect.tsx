8fc7fe6e3876e3392cca0631748bea1a0c101387
laura <mbernallaura@gmail.com>
2025-11-28 10:43:59 -0500

    se termina el semaforo con el hook useEffect ademas se agrega barra de progreso

--- src/02-useEffect/TrafficLightWithEffect.tsx
+++ src/02-useEffect/TrafficLightWithEffect.tsx
@@ -12,6 +12,7 @@ export const TrafficLightWithEffect = () => {
     const [light, setLight] = useState<TrafficLightColor>("red");
     const [countdown, setCountdown] = useState(5);
 
+    //? Countdown effect
     useEffect(() => {
         if(countdown === 0) return;
 
@@ -27,11 +28,42 @@ export const TrafficLightWithEffect = () => {
     }, [countdown])
     
 
+    //?Change light color effect
+    useEffect(() => {
+        if (countdown > 0) return;
+
+        setCountdown(5);
+
+        switch (light) {
+            case 'red':
+                setLight('green');
+                break;
+
+            case 'yellow':
+                setLight('red');
+                break;
+            
+            case 'green':
+                setLight('yellow');
+                break;
+            
+            default:
+                break;
+        }
+    }, [countdown, light])
+    
+
     return (
         <div className="min-h-screen bg-gradient-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
         <div className="flex flex-col items-center space-y-8">
             <h1 className="text-white text-3xl font-thin">Semaforo con UseEffect</h1>
             <h2 className="text-white text-xl">Countdown: { countdown }</h2>
+            <div className="w-64 bg-gray-700 rounded-full h-2">
+                <div 
+                    className=" bg-blue-500 h-2 rounded-full transition-all duration-1000 ease-linear"
+                    style={{ width: `${ (countdown/5) * 100 }%` }}
+                ></div>
+            </div>
 
             <div className={`w-32 h-32 ${ light ==='red' ? colors[light] : "bg-gray-500" } rounded-full`}></div>
             <div className={`w-32 h-32 ${ light ==='yellow' ? colors[light] : "bg-gray-500" } rounded-full`}></div>
