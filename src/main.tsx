55e6c812788af9f11170de8273988ab3e7cc8a1a
laura <mbernallaura@gmail.com>
2025-12-01 11:36:22 -0500

    s

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 // import { TrafficLight } from "./01-useState/TrafficLight";
 //import { TrafficLightWithEffect } from "./02-useEffect/TrafficLightWithEffect";
-import { TrafficLightWithHook } from "./02-useEffect/TrafficLightWithHook";
+// import { TrafficLightWithHook } from "./02-useEffect/TrafficLightWithHook";
+import { PokemonPage } from "./03-examples/PokemonPage";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     {/* <HooksApp/> */}
     {/* <TrafficLight /> */}
     {/* <TrafficLightWithEffect/> */}
-    <TrafficLightWithHook/>
+    {/* <TrafficLightWithHook/> */}
+    <PokemonPage/>
   </StrictMode>
 );
