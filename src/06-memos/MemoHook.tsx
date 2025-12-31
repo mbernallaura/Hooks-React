cda4575eaf1170e1514b38d64445abbcad80d08b
laura <mbernallaura@gmail.com>
2025-12-31 11:52:29 -0500

    comprendiendo el funcionaiento del useCallback y el React.memo con un ejercicio sencillo

--- src/06-memos/MemoHook.tsx
+++ src/06-memos/MemoHook.tsx
@@ -1,18 +1,29 @@
-import { useState } from "react"
+import { useCallback, useState } from "react"
 import { MyTitle } from "./ui/MyTitle"
 import { MySubtitle } from "./ui/MySubtitle";
 
+//! Si la funcion esta fuera, no se renderiza, no cambia entre re-renders
+    const handleMyApiCall = (myValue: string)=>{
+        console.log('llamar a mi api - ');
+    };
 
 export const MemoHook = () => {
     const [title, setTitle] = useState('Hola');
-    const [subtitle, setSubtitle] = useState('Mundo')
+    const [subtitle, setSubtitle] = useState('Mundo');
+
+    //!Si se tiene funciones dentro de un componente, la funcion define un nuevo espacio en memoria cada vez que se recargue el commponente por ende
+    //!se usa el useCallback para memorizar esa funcion y no cree nuevos espacios en memoria
+    // const handleMyAPICall = useCallback(()=>{
+    //     console.log('llamar a mi api - ', subtitle);
+        
+    // }, [subtitle]);
 
     return (
         <div className="bg-gradient flex flex-col gap-4">
             <h1 className="text-2xl font-thin text-white">MemoApp</h1>
 
             <MyTitle title={title}/>
-            <MySubtitle subtitle={subtitle}/>
+            <MySubtitle subtitle={subtitle} callMyAPI={handleMyApiCall}/>
 
             <button 
                 className="bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer"
--- src/06-memos/ui/MySubtitle.tsx
+++ src/06-memos/ui/MySubtitle.tsx
@@ -1,15 +1,19 @@
 import { memo } from "react";
 
 interface Props{
-    subtitle:string
+    subtitle:string;
+    callMyAPI: (myValue: string) => void;
 }
 
-export const MySubtitle = memo(({subtitle}:Props) => {
+export const MySubtitle = memo(({subtitle, callMyAPI}:Props) => {
     console.log('MySubtitle re-render');
     return (
         <>
             <h6 className="text-2xl font-bold">{subtitle}</h6>
-            <button className="bg-indigo-500 text-white px-2 py-1 rounded-md cursor-pointer">Llamar a funcion</button>
+            <button 
+                className="bg-indigo-500 text-white px-2 py-1 rounded-md cursor-pointer"
+                onClick={() => callMyAPI(subtitle)}
+            >Llamar a funcion</button>
         </>
     )
 });
