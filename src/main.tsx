e98ed183075d72d2b711f34939854623c4e956df
laura <mbernallaura@gmail.com>
2026-01-02 17:30:10 -0500

    agregar el memocounter en el main

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 //import { FocusScreen } from "./04-useRef/FocusScreen";
 // import { TasksApp } from "./05-useReducer/TaskApp";
 // import { ScrambleWords } from "./05-useReducer/ScrambleWords";
-import { MemoHook } from "./06-memos/MemoHook";
+// import { MemoHook } from "./06-memos/MemoHook";
 import { MemoCounter } from "./06-memos/MemoCounter";
 // import { TrafficLight } from "./01-useState/TrafficLight";
 //import { TrafficLightWithEffect } from "./02-useEffect/TrafficLightWithEffect";
